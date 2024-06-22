9cb4a4088cf80835f6271a6abd234e78ac16e4e2
Zak Nowshadi <zn26@st-andrews.ac.uk>
2024-06-22 11:36:03 +0100

    Rejigged the fiel structure

--- BirdWalker.py
+++ BirdWalker.py
@@ -6,7 +6,7 @@ import BaseGraphGenerator
 # Making the bird walk from the bottom left corner to the top right corner
 
 def main(sizeofGraph):
-    # The bird walker will start at the bottom left corner
+    # The bird walker will start in the bottom left corner
     x = 0
     y = 0
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Running the base graph generator and then putting the birdwalker on top of it
-import BaseGraphGenerator
 import BirdWalker
 
 # Defining the size of the graph

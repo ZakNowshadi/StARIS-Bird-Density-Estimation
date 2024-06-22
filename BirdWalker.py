eb025dcaa62924db2a115b46ac8b603934c37e9b
Zak Nowshadi <zn26@st-andrews.ac.uk>
2024-06-22 12:10:14 +0100

    Got dot walking across the screen

--- BirdWalker.py
+++ BirdWalker.py
@@ -6,6 +6,8 @@ import BaseGraphGenerator
 # Making the bird walk from the bottom left corner to the top right corner
 
 def main(sizeofGraph):
+    plt.ion()
+
     # The bird walker will start in the bottom left corner
     x = 0
     y = 0
@@ -13,11 +15,9 @@ def main(sizeofGraph):
     # The bird walker will move to the top right corner
     while x < sizeofGraph and y < sizeofGraph:
         plt.plot(x, y, 'bo')
-        # Making the base graph
         BaseGraphGenerator.main(sizeofGraph)
-        plt.show()
-        plt.close()
+        plt.draw()
+        plt.pause(0.3)  # short pause
+        plt.close('all')
         x += 1
         y += 1
-
-

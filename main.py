bbaf6956b40f784df65fa8fa78216ff5d2c10ae0
Zak Nowshadi <zn26@st-andrews.ac.uk>
2024-06-22 11:21:16 +0100

    Updated the main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from matplotlib import pyplot as plt
+# Running the base graph generator and then putting the birdwalker on top of it
+import BaseGraphGenerator
+import BirdWalker
 
-x = 10
-y = 10
+# Defining the size of the graph
+sizeofGraph = 10
+BaseGraphGenerator.main(sizeofGraph)
 
-# Make a graph of x by y
-# Placing a purple dot in the center to represent the sensor
-plt.plot(x/2, y/2, 'mo')
+BirdWalker.main()
 
-# Placing a red dot in each corner
-plt.plot(0, 0, 'mo')
-plt.plot(0, y, 'mo')
-plt.plot(x, 0, 'mo')
-plt.plot(x, y, 'mo')
-
-plt.show()

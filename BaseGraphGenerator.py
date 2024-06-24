f23cc2e00e7875799bd1828f60b2abf419265571
Zak Nowshadi <zn26@st-andrews.ac.uk>
2024-06-24 14:46:17 +0100

    Now working with a single tile - no audio though

--- BaseGraphGenerator.py
+++ BaseGraphGenerator.py
@@ -1,25 +1,17 @@
 from matplotlib import pyplot as plt
-from matplotlib.lines import Line2D
 
 
 # A sensor class
 class SensorZone:
-    def __init__(self, x, y, isCentralSensor):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.radius = 3.5
-        self.isCentralSensor = isCentralSensor
-        self.centreOfZoneX = None
-        self.centreOfZoneY = None
-        if isCentralSensor:
-            # Set the centre dot to the centre of plot
-            self.centreOfZoneX = x
-            self.centreOfZoneY = y
-        else:
-            # Set the centre dot to the halfway point between centre and its radius, for the corner sensors only
-            # The dot should sit on a line between the centre sensor and the corner sensor
-            self.centreOfZoneX = (x + (x / 2)) / 2
-            self.centreOfZoneY = (y + (y / 2)) / 2
+        halfRadius = self.radius / 2
+        self.northWestDot = (x - halfRadius, y + halfRadius)
+        self.northEastDot = (x + halfRadius, y + halfRadius)
+        self.southWestDot = (x - halfRadius, y - halfRadius)
+        self.southEastDot = (x + halfRadius, y - halfRadius)
 
     # Getter for x
     def getX(self):
@@ -37,18 +29,18 @@ class SensorZone:
     def setY(self, y):
         self.y = y
 
-    def checkIfCentralSensor(self):
-        return self.isCentralSensor
-
     def drawSensorZone(self):
         # Plotting the sensor itself
-        plt.plot(self.x, self.y, 'mp', label='sensor')
+        plt.plot(self.x, self.y, 'mp')
         # Draw a green of circle around each sensor
         circle = plt.Circle((self.x, self.y), self.radius, color='g', fill=False)
         plt.gca().add_artist(circle)
 
-        # Putting a red dot a halfway point between the centre and its radius
-        plt.plot(self.centreOfZoneX, self.centreOfZoneY, 'ro', label='centre of sensor zone')
+        # Drawing the dots that represent the corners of the sensor zone
+        plt.plot(self.northWestDot[0], self.northWestDot[1], 'ro')
+        plt.plot(self.northEastDot[0], self.northEastDot[1], 'ro')
+        plt.plot(self.southWestDot[0], self.southWestDot[1], 'ro')
+        plt.plot(self.southEastDot[0], self.southEastDot[1], 'ro')
 
 
 def main(maxSize):
@@ -60,15 +52,13 @@ def main(maxSize):
     # Make a graph of x by y
     # Placing a purple dot in the center to represent the sensor
     # Making the centre sensor object
-    centreSensor = SensorZone(X / 2, Y / 2, True)
-    plt.plot(centreSensor.getX(), centreSensor.getY(), 'mp', label='sensor')
+    centreSensor = SensorZone(X / 2, Y / 2)
 
     # Making the corner sensor objects
-    upperLeftSensor = SensorZone(0, 0, False)
-    upperRightSensor = SensorZone(0, Y, False)
-    lowerLeftSensor = SensorZone(X, 0, False)
-    lowerRightSensor = SensorZone(X, Y, False)
-
+    upperLeftSensor = SensorZone(0, 0,)
+    upperRightSensor = SensorZone(0, Y)
+    lowerLeftSensor = SensorZone(X, 0)
+    lowerRightSensor = SensorZone(X, Y)
 
     # Drawing each sensor zone's radius
     # Draw a green of circle around each sensor

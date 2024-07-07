f74df124c63da0c4eace6596608a56243b4876ca
Zak Nowshadi <zn26@st-andrews.ac.uk>
2024-07-07 12:59:54 +0100

    Animation is now implemented, so it does not re-render the whole plot each time. But there is a strange centeral bird image that just stays there.

--- BirdWalker.py
+++ BirdWalker.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
-
+from matplotlib.animation import FuncAnimation
 import AudioManipulator
 import BaseGraphGenerator
 
@@ -9,15 +9,17 @@ import BaseGraphGenerator
 
 # Making a bird class
 class Bird:
-    def __init__(self, x, y):
+    def __init__(self, x, y, imagePath):
         self.x = x
         self.y = y
-        self.image = mpimg.imread('Images/robin.png')
+        self.image = mpimg.imread(imagePath)
         # Will be null if in no sensor zone
         # Needs to be know so know which sensor to play the sound to as
         # Under current implementation only one sensor should hear a bird at a time
         self.currentSensorZone = None
         self.distanceFromSensor = 0
+        # The artist object for the bird
+        self.artist = None
 
     # Getter for x
     def getX(self):
@@ -27,16 +29,15 @@ class Bird:
     def getY(self):
         return self.y
 
-    # Setter for x
-    def setX(self, x):
-        self.x = x
+    # def draw(self):
+    #     plt.imshow(self.image, extent=[self.x, self.x + 1, self.y, self.y + 1])
 
-    # Setter for y
-    def setY(self, y):
-        self.y = y
-
-    def draw(self):
-        plt.imshow(self.image, extent=[self.x, self.x + 1, self.y, self.y + 1])
+    def draw(self, ax):
+        extent = [self.x, self.x + 1, self.y, self.y + 1]
+        if self.artist is None:
+            self.artist = ax.imshow(self.image, extent=extent)
+        else:
+            self.artist.set_extent(extent)
 
     def sensorZoneCheck(self):
         # Checking if the bird is in a sensor zone
@@ -51,44 +52,74 @@ class Bird:
                 # If the bird is in a sensor zone
                 self.currentSensorZone = sensorZone
                 self.distanceFromSensor = distance
+                print("Bird is in a sensor zone")
+                print("Sensor zone coords: ", sensorZone.getX(), sensorZone.getY())
+                print("Distance from sensor: ", distance)
                 return True
         # If the bird is not in a sensor zone
         self.currentSensorZone = None
         self.distanceFromSensor = 0
+        print("Bird is NOT in a sensor zone")
         return False
+
+    def updatePosition(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def update(frame, bird, ax, sizeofGraph):
+    # Incrementing the coords of the bird
+    newX, newY = bird.getX() + 1, bird.getY() + 1
+    # Ensure bird stays within graph bounds
+    bird.updatePosition(newX % sizeofGraph, newY % sizeofGraph)
+    # Printing current coords of the bird
+    print("\n-----------------------")
+    print("Bird is at: ", bird.getX(), bird.getY())
+
+    bird.draw(ax)
+    if bird.sensorZoneCheck():
+        # Passing the bird to the AudioManipulator
+        # Where the frame acts as the count
+        AudioManipulator.main(bird, frame)
+
+    return bird.artist,
 
 
 def main(sizeofGraph):
-    plt.ion()
+    # Setting up the plot
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, sizeofGraph)
+    ax.set_ylim(0, sizeofGraph)
 
     # The bird walker will start in the bottom left corner
     # Making a new bird object
-    bird1 = Bird(0, 0)
+    bird1 = Bird(0, 0, 'Images/robin.png')
+    # Drawing the static background only once
+    BaseGraphGenerator.main(sizeofGraph)
 
-    numberOfTimesInSensorZone = 0
-    # The bird walker will move to the top right corner
-    while bird1.getX() <= sizeofGraph and bird1.getY() <= sizeofGraph:
-        # Drawing the bird
-        bird1.draw()
-        BaseGraphGenerator.main(sizeofGraph)
-        plt.tight_layout()
-        plt.draw()
-        plt.pause(0.5)  # short pause
-        plt.close('all')
-        # Printing the current coords of the sensor zone it is in
-        print("Bird is at: ", bird1.getX(), bird1.getY())
-        # Checking if the bird is in a sensor zone
-        print("\n--------------------")
-        if bird1.sensorZoneCheck():
-            print("Bird is in a sensor zone")
-            print("Distance from sensor: ", bird1.distanceFromSensor)
-            print("Sensor zone coords: ", bird1.currentSensorZone.getX(), bird1.currentSensorZone.getY())
-            # Passing the bird to the AudioManipulator
-            AudioManipulator.main(bird1, numberOfTimesInSensorZone)
-            numberOfTimesInSensorZone += 1
-        else:
-            print("Bird is NOT in a sensor zone")
+    # Running the animation - ensuring that the garbage collector does not delete the animation object
+    ani = FuncAnimation(fig, update, fargs=(bird1, ax, sizeofGraph), frames=range(sizeofGraph), blit=True)
 
-        # Incrementing the coords of the bird
-        bird1.setX(bird1.getX() + 1)
-        bird1.setY(bird1.getY() + 1)
+    plt.show()
+    # numberOfTimesInSensorZone = 0
+    # # The bird walker will move to the top right corner
+    # while bird1.getX() <= sizeofGraph and bird1.getY() <= sizeofGraph:
+    #     # Drawing the bird
+    #     bird1.draw()
+    #     BaseGraphGenerator.main(sizeofGraph)
+    #     plt.tight_layout()
+    #     plt.draw()
+    #     plt.pause(0.5)  # short pause
+    #     plt.close('all')
+    #     # Printing the current coords of the sensor zone it is in
+    #     print("Bird is at: ", bird1.getX(), bird1.getY())
+    #     # Checking if the bird is in a sensor zone
+    #     print("\n--------------------")
+    #     if bird1.sensorZoneCheck():
+    #         # Passing the bird to the AudioManipulator
+    #         AudioManipulator.main(bird1, numberOfTimesInSensorZone)
+    #         numberOfTimesInSensorZone += 1
+    #     else:
+    #
+    #     # Incrementing the coords of the bird
+    #     bird1.updatePosition(bird1.getX() + 1, bird1.getY() + 1)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Running the base graph generator and then putting the birdwalker on top of it
+# Running the base graph generator and then putting the bird walker on top of it
 import BirdWalker
 
 # Defining the size of the graph
